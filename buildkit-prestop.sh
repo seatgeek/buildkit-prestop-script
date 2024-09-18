87b032480e289d513be6d753d5a6932c50fcd25a
PidgeyBE <pieterjan@peej.ai>
2024-09-18 14:42:02 +0200

    Print prestop logs to pod logs

--- buildkit-prestop.sh
+++ buildkit-prestop.sh
@@ -40,14 +40,20 @@ REQUIRED_CHECK_COUNT=$((WAIT_UNTIL_NO_BUILDS_SEEN_FOR_X_SECONDS * 1000 / CHECK_F
 # How many consecutive times we've seen no running builds
 times=0
 
+# Print logs both locally as in pod logs
+print_logs() {
+    echo $1
+    echo "PreStop Hook: $1" >> /proc/1/fd/1
+}
+
 # Function to print a message conditionally based on DEBUG environment variable
 print_debug() {
     if [ -n "$DEBUG" ]; then
-        echo "$1"
+        print_logs "$1"
     fi
 }
 
-echo "Waiting for build processes to finish..."
+print_logs "Waiting for build processes to finish..."
 
 # Loop until we see zero active connections REQUIRED_CHECK_COUNT consecutive times
 while true; do
@@ -64,4 +70,4 @@ while true; do
     usleep $CHECK_FREQUENCY_MS"000"  # Sleep for CHECK_FREQUENCY_MS milliseconds before checking again
 done
 
-echo "All build processes seem to have stopped. Exiting now."
+print_logs "All build processes seem to have stopped. Exiting now."
